7301619b827519105583ce43569cb9c1294a88f5
Honey <honeysrk72@gmail.com>
2022-02-03 21:37:43 +0530

    Perform nested try except

--- exception_handling/try_excepts.py
+++ exception_handling/try_excepts.py
@@ -2,17 +2,24 @@
 Try, except, else, finally
 """
 
+from decimal import DivisionByZero
+
+
 def main():
     """Main function"""
 
     try:
-        a = int(input("Enter first integer: "))
-        b = int(input("Enter second integer: "))
-    except ValueError:
-        print("Please enter an integer value")
+        try:
+            a = int(input("Enter first integer: "))
+            b = int(input("Enter second integer: "))
+        except ValueError:
+            print("Please enter an integer value")
+        else:
+            result = a / b
+    except ZeroDivisionError:
+        print("Can't be divided by zero")
     else:
-        result = a + b
-        print(f"{a} + {b} = {result}")
+        print(f"{a} / {b} = {result}")
     finally:
         print("I'll always run!")
 
