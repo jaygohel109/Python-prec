f93e1366e87cf81366351a35c2e0bd9901b3d864
Gohel Jay <56113257+jaygohel109@users.noreply.github.com>
2024-03-18 14:19:47 -0500

    Update test_task_1.py
    updated test file

--- pytest/test_task_1.py
+++ pytest/test_task_1.py
@@ -1,18 +1,27 @@
-"""
-Test file for task 1
-"""
-
 import pytest
-from task_1 import nth_fibonacci
+from your_module import nth_fibonacci  # Replace 'your_module' with the actual name of your python file containing the nth_fibonacci function
 
-@pytest.mark.parametrize('nth_fibonacci, num', [(1, 1), (2, 1), (3, 2), (4, 3)])
-def test_value(nth_fibonacci, num):
-    assert nth_fibonacci(nth_fibonacci) == num
+def test_fibonacci_positive():
+    # Testing positive cases
+    assert nth_fibonacci(1) == 0, "The 1st Fibonacci number should be 0"
+    assert nth_fibonacci(2) == 1, "The 2nd Fibonacci number should be 1"
+    assert nth_fibonacci(3) == 1, "The 3rd Fibonacci number should be 1"
+    assert nth_fibonacci(4) == 2, "The 4th Fibonacci number should be 2"
+    assert nth_fibonacci(5) == 3, "The 5th Fibonacci number should be 3"
+    assert nth_fibonacci(6) == 5, "The 6th Fibonacci number should be 5"
+    # Add more tests as needed
 
-@pytest.mark.parametrize('num', [-2, 0])
-def test_error(num):
+def test_fibonacci_edge_case():
+    # Testing edge cases
+    assert nth_fibonacci(10) == 34, "The 10th Fibonacci number should be 34"
+    # Add more edge cases as needed
+
+def test_fibonacci_invalid():
+    # Testing for invalid input
     with pytest.raises(ValueError):
-        nth_fibonacci(num)
+        nth_fibonacci(0)
+    with pytest.raises(ValueError):
+        nth_fibonacci(-1)
+    # Add more invalid inputs as needed
 
-if __name__ == '__main__':
-    pytest.main()
+# Add more test functions as required
